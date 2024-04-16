561f5e693de0d73e132c87dfdbc889f5a63efd50
ElBookyes <dtuparov94@gmail.com>
2024-04-16 11:27:24 +0300

    main
    completed the menu

--- hello-world-2201681063/src/app/app.component.ts
+++ hello-world-2201681063/src/app/app.component.ts
@@ -16,13 +16,17 @@ export class AppComponent {
     { title: 'За мен', 
       content: 'Аз съм Бобката ама някой ми викат Гопката. Топ 15 съм в CS 1.6 и съм roblox милионер.', 
       isOpen: false,
-   },
-    { title: 'Хобита и интереси', content: '12 часа от денят ми отиват в тежка гейминг сесия, а другите 4 си варя яйца.', isOpen: false },
+    },
+
+    { title: 'Хобита и интереси', content: '12 часа от денят ми отиват в тежка гейминг сесия, а другите 4 си варя яйца.', 
+      isOpen: false },
+
     { title: 'Любимият ми content creator', 
       content: 'CDawgVA - The Monke', 
       isOpen: false,
       link: 'https://www.youtube.com/@CDawgVA',
       linkdesc: 'Channel Link',},
+
     { title: 'Информация за канала ми', 
       content: 'Youtube: BobkataGopkov', 
       isOpen: false,
@@ -30,10 +34,18 @@ export class AppComponent {
         'В CS 1.6 правя неща, които никой не е виждал до ся.',
         'Всеки петък в 11:00 се пускам на живо и ви показвам как да си спретнете най-тежката попара в целия свят.'
       ] },
-    { title: 'Примерно остойностяване', content: 'Ако искате да колабваме или да работим заедно плащам единствено в exposure.', isOpen: false }
+
+    { title: 'Примерно остойностяване', 
+      content: 'Ако искате да колабваме или да работим заедно плащам единствено в exposure.', 
+      isOpen: false }
   ];
 
   toggleItem(index: number) {
+    this.menuItems.forEach((item, i) => {
+      if (i !== index) {
+        item.isOpen = false;
+      }
+    });
     this.menuItems[index].isOpen = !this.menuItems[index].isOpen;
   }
 }
